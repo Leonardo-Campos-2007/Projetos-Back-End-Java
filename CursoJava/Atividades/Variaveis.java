b6ee0b9e553c44a7995ca2a7478fd340b4dc3d53
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2025-11-01 19:29:51 -0300

    Variaveis em Java

---
+++ CursoJava/Atividades/Variaveis.java
@@ -0,0 +1,18 @@
+public class Variaveis {
+
+    public static void main(String[] args) {
+
+        int idade = 25;
+        String nome = "João";
+        double salario = 2500.50;
+        boolean empregado = true;
+
+        System.out.println("Meu nome eh Joao: " + nome);
+        System.out.println("Minha idade eh de " + idade);
+        System.out.println("Tenho  um salario de : R$" + salario);
+        System.out.println("Estou empregado? " + empregado);
+   
+}
+
+}
+ 
