03a66802e98b974bb75e2c6fff810be32253d78c
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2025-11-02 18:57:11 -0300

    if

--- CursoJava/Atividades/IF.java
+++ CursoJava/Atividades/IF.java
@@ -1,20 +1,104 @@
+import java.util.Scanner;
+
 public class IF {
     public static void main(String[] args) {
 
-        adulto();
-        
+        // adulto();
+
+        // notas();
+
+        // scanner();
+
+        votar();
+
     }
 
-    public static void adulto(){
+    public static void adulto() {
 
         int idade = 18;
 
-        if(idade >= 18){
+        if (idade >= 18) {
 
             System.out.println("Você eh de maior");
 
         }
     }
 
-    public static void 
+    public static void notas() {
+
+        double nota = 1.0;
+
+        if (nota >= 7.0) {
+
+            System.out.println("Está aprovado na matéria");
+
+        }
+
+        else if (nota >= 5.0) {
+
+            System.out.println("Esta de recuperação");
+
+        }
+
+        else {
+
+            System.out.println("Esta reprovado");
+        }
+
+    }
+
+    public static void scanner() {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite a nota  do aluno: ");
+
+        double nota = entrada.nextDouble();
+
+        if (nota >= 7.0) {
+
+            System.out.println("Está aprovado na matéria");
+
+        }
+
+        else if (nota >= 5.0) {
+
+            System.out.println("Esta de recuperação");
+
+        }
+
+        else {
+
+            System.out.println("Esta reprovado");
+        }
+
+        entrada.close();
+
+    }
+
+    public static void votar() {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Qual sua idade: ");
+
+        int idade = entrada.nextInt();
+
+        entrada.nextLine();
+
+        System.out.println("Voce eh brasileiro ou naturalizado? (sim/nao)");
+
+        String nacionalidade = entrada.nextLine().toLowerCase();
+
+        if (idade >= 16 && nacionalidade.equals("sim")) {
+
+            System.out.println("Voce pode votar");
+
+        } else {
+            System.out.println("Voce  não pode votar");
+        }
+
+        entrada.close();
+
+    }
 }
