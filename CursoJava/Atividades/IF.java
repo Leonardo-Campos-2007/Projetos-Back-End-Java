c35826dea39e14cd72dd7df4dda4768e4d3f3c4d
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2025-11-02 17:03:48 -0300

    IF

---
+++ CursoJava/Atividades/IF.java
@@ -0,0 +1,20 @@
+public class IF {
+    public static void main(String[] args) {
+
+        adulto();
+        
+    }
+
+    public static void adulto(){
+
+        int idade = 18;
+
+        if(idade >= 18){
+
+            System.out.println("Você eh de maior");
+
+        }
+    }
+
+    public static void 
+}
