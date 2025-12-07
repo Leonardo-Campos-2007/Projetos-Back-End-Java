b5c3552dd7b947dd002653a4a71c6fefed720a3b
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2025-12-07 16:44:16 -0300

    Switch

--- CursoJava/Atividades/Switch.java
+++ CursoJava/Atividades/Switch.java
@@ -4,7 +4,9 @@ public class Switch {
 
     public static void main(String[] args) {
 
-        cores();
+       //cores();
+
+       MarcarConsulta();
 
     }
 
@@ -45,6 +47,71 @@ public class Switch {
         }
 
         entrada.close();
+
+    }
+
+    public static void MarcarConsulta(){
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite o dia da semana que desaja marcar um horario: ");
+
+        System.out.println("1-Domingo");
+        System.out.println("2-Segunda");
+        System.out.println("3-Terca");
+        System.out.println("4-Quarta");
+        System.out.println("5-Quinta");
+        System.out.println("6-Sexta");
+        System.out.println("7-Sabado");
+
+        System.out.print("Qual dia deseja: ");
+        int opcao = entrada.nextInt();
+
+        switch (opcao) {
+            case 1:
+
+            System.out.println("A consulta foi marcada para Domingo");
+                
+                break;
+            case 2:
+
+            System.out.println("A consulta foi marcada para Segunda");
+                
+                break;
+            case 3:
+
+            System.out.println("A consulta foi marcada para Terca");
+                
+                break;
+            case 4:
+
+            System.out.println("A consulta foi marcada para Quarta");
+                
+                break;
+            case 5:
+
+            System.out.println("A consulta foi marcada para Quinta");
+                
+                break;
+            case 6:
+
+            System.out.println("A consulta foi marcada para Sexta");
+                
+                break;
+            case 7:
+
+            System.out.println("A consulta foi marcada para Sabado");
+                
+                break;
+        
+            default:
+
+            System.out.println("Opcao invalida");
+                break;
+        }
+
+        entrada.close();
+
 
     }
 
