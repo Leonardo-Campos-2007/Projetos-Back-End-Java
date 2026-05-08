fd1e9c7aa918d4c24094ced2ff73939dbd2478d1
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-05-08 15:24:49 -0300

    fix: Downgrade Spring Boot version to 3.4.5 and update Java version to 21

--- API REST SPRING/br/src/main/java/com/api/br/controller/GreetingController.java
+++ API REST SPRING/br/src/main/java/com/api/br/controller/GreetingController.java
@@ -1,8 +1,10 @@
 package com.api.br.controller;
 
+import com.api.br.model.Greeting;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
     
 }
+
