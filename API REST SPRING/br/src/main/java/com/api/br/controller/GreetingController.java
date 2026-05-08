969c34176bec186efeb461e2699eee924d2e0a1d
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-05-08 16:42:45 -0300

    feat: Implement GreetingController with greeting endpoint for personalized greetings

--- API REST SPRING/br/src/main/java/com/api/br/controller/GreetingController.java
+++ API REST SPRING/br/src/main/java/com/api/br/controller/GreetingController.java
@@ -1,10 +1,27 @@
 package com.api.br.controller;
 
 import com.api.br.model.Greeting;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+        @RequestParam(value = "name", defaultValue = "Word")
+        String name){
+            return new Greeting(counter.incrementAndGet(), String.format(template, name));
+
+    }
     
 }
 
