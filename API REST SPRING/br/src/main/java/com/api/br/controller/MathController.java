af34e2de8eb261ab23c007b24459d73f799e3cbc
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-05-11 10:08:52 -0300

    feat: Update error handling in MathController to use UnsupportedMathOperationException for numeric validation

--- API REST SPRING/br/src/main/java/com/api/br/controller/MathController.java
+++ API REST SPRING/br/src/main/java/com/api/br/controller/MathController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.br.exception.UnsupportedMathOperationException;
+
 @RestController
 @RequestMapping("/math")
 public class MathController {
@@ -14,16 +16,16 @@ public class MathController {
             @PathVariable("numberTwo") String numberTwo) throws Exception {
 
         if (!isNumeric(numberOne) || !isNumeric(numberTwo))
-            throw new IllegalArgumentException();
+            throw new UnsupportedMathOperationException("Insira um valor numerico");
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
-    private Double convertToDouble(String strNumber) throws IllegalArgumentException{
+    private Double convertToDouble(String strNumber) throws IllegalArgumentException {
         if (strNumber == null || strNumber.isEmpty())
-            throw new IllegalStateException();
+            throw new UnsupportedMathOperationException("Insira um valor numerico");
         String number = strNumber.replace(",", ".");
         return Double.parseDouble(strNumber);
-        
+
     }
 
     private boolean isNumeric(String strNumber) {
