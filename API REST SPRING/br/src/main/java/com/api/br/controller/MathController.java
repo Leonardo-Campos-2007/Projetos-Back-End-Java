884cdbade67aa11c70c0171475af692e150f5c9a
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-05-11 08:49:31 -0300

    feat: Enhance sum endpoint in MathController to validate numeric inputs

--- API REST SPRING/br/src/main/java/com/api/br/controller/MathController.java
+++ API REST SPRING/br/src/main/java/com/api/br/controller/MathController.java
@@ -11,8 +11,19 @@ public class MathController {
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(
             @PathVariable("numberOne") String numberOne,
-            @PathVariable("numberTwo") String numberTwo) {
+            @PathVariable("numberTwo") String numberTwo
+        ) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo) ) throw new IllegalArgumentException();
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String number){
         return 1D;
+    }
+
+    private boolean isNumeric(String number){
+        return true;
     }
 
 }
