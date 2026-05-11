ed2ae23fcce8fd71a41bdb03a2bb6cc21550a42d
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-05-11 09:28:35 -0300

    feat: Add ExceptionResponse and UnsupportedMathOperationException classes for error handling

---
+++ API REST SPRING/br/src/main/java/com/api/br/exception/UnsupportedMathOperationException.java
@@ -0,0 +1,7 @@
+package com.api.br.exception;
+
+public class UnsupportedMathOperationException extends RuntimeException{
+    public UnsupportedOperationException(String message){super(message);}
+    
+     
+}
